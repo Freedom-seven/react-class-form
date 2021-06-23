994923fb7ca523a39fca398e5d42016548576ce4
FREEDOM <freewhynane62@gmail.com>
2021-06-23 08:03:50 +0000

    add component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import ClassForm from "./Components/ClassForm/ClassForm";
+import FunctionForm from "./Components/FunctionForm/FunctionForm";
 
 
 function App() {
   return (
     <>
-      
+      <ClassForm />
+      <FunctionForm />
     </>
   );
 }
