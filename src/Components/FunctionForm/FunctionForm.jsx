6b54471874ae44d2a8c6a3715420f9e900dc2eb4
FREEDOM <freewhynane62@gmail.com>
2021-06-23 11:30:31 +0000

    add react hook

--- src/Components/FunctionForm/FunctionForm.jsx
+++ src/Components/FunctionForm/FunctionForm.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './FunctionForm.css'
 
 const FunctionForm = () => {
+    const [form, setForm] = useState({
+        username: "",
+        email: "",
+        password: ""
+    });
     return (
         <div>
             Function Form
