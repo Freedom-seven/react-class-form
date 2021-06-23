d9e33a0c86365eab1d2cec4246ece7dbe58ab846
FREEDOM <freewhynane62@gmail.com>
2021-06-23 08:03:20 +0000

    testing

---
+++ src/Components/ClassForm/ClassForm.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ClassForm extends Component {
+    render() {
+        return (
+            <div>
+                Class Form
+            </div>
+        );
+    }
+}
+
+export default ClassForm;
