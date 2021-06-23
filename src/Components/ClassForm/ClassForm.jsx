dfea211d16d04e67f664068b3d357f3057bf7ce1
FREEDOM <freewhynane62@gmail.com>
2021-06-23 08:20:02 +0000

    add handles

--- src/Components/ClassForm/ClassForm.jsx
+++ src/Components/ClassForm/ClassForm.jsx
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 
 class ClassForm extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {value: ""};
+
+        this.handleChnage = this.handleChnage.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChnage(e) {
+        this.setState({value: e.target.value});
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log("A class form filled: " + this.state.value);
+    }
+
     render() {
         return (
             <div>
